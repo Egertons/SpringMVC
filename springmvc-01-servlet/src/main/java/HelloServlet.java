9df7e29c1b749902147914cf0d4ca3847f00d160
egert <egertons@163.com>
2021-02-13 15:48:03 +0800

    2021_2_13_15_47

---
+++ springmvc-01-servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.获取前端参数
+        //2.调用业务层
+        //3.视图转发或者重定向
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
